82ac8608e038be7d49636ee31592d32891c3b1ee
livemonkey1300 <39892162+livemonkey1300@users.noreply.github.com>
2019-04-10 16:45:13 -0400

    Update run.sh
    sss

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+source ./runner
 pip install --upgrade virtualenv
 virtualenv env
 source env/bin/activate
@@ -7,3 +8,5 @@ pip install djangocms-installer
 djangocms main
 python main/manage.py makemigrations
 python main/manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='"$EMAIL"', is_superuser=True).delete(); User.objects.create_superuser('"$ADMIN"', '"$EMAIL"', '"$ADMIN_PASS"')" | /bin/bash -c "python main/manage.py shell" 
+sed -i -e 's/ALLOWED_HOSTS\ \=\ \[\]/ALLOWED_HOSTS\ \=\ \["*"\]/g' main/main/settings.py
