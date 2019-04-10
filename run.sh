15bb9bb3c48a8054b7be2926953cf60725baad1d
livemonkey1300 <39892162+livemonkey1300@users.noreply.github.com>
2019-04-10 16:47:06 -0400

    ssss
    yes

--- run.sh
+++ run.sh
@@ -10,3 +10,4 @@ python main/manage.py makemigrations
 python main/manage.py migrate
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='"$EMAIL"', is_superuser=True).delete(); User.objects.create_superuser('"$ADMIN"', '"$EMAIL"', '"$ADMIN_PASS"')" | /bin/bash -c "python main/manage.py shell" 
 sed -i -e 's/ALLOWED_HOSTS\ \=\ \[\]/ALLOWED_HOSTS\ \=\ \["*"\]/g' main/main/settings.py
+
